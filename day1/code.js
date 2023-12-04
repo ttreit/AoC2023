a049202f5cd4a35ed94951ecaff10e6b4ba81e75
ttreit <14933277+ttreit@users.noreply.github.com>
2023-12-04 11:22:45 -0800

    Change variable names

--- day1/code.js
+++ day1/code.js
@@ -2,16 +2,16 @@ function simpleFunction() {
     console.log('I am a simple function');
 }
 
-function findfirstCharacter() {
+function findFirstNumber() {
     //Variables
         let input;
-        let firstCharacter = '0';
-        let firstNumber = 0;
+        let extractedCharacters = '';
+        let extractedNumber = 0;
         let iterator = 0;
-        const returnArray = [firstNumber, iterator];
+        const returnArray = [extractedNumber, iterator];
         const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-        input = '1212b';
+        input = '13b';
     
         for (let i = 0; i < input.length; i++) {
             console.log('input ', input);
@@ -19,36 +19,36 @@ function findfirstCharacter() {
             console.log('iCharacter ', iCharacter);
             if (numbers.some(x => iCharacter.includes(x))) {
                 console.log('iCharacter contains a number');
-                firstCharacter = firstCharacter.concat(iCharacter);
-                console.log('firstCharacter ', firstCharacter, typeof firstCharacter);
-            } else if (firstCharacter === '0') {
+                extractedCharacters = extractedCharacters.concat(iCharacter);
+                console.log('extractedCharacters ', extractedCharacters, typeof extractedCharacters);
+            } else if (extractedCharacters === '') {
                 continue;
-            } else if (true) {
-                console.log('TRUE FALSE ELSE IF');
-                console.log('firstCharacter ', firstCharacter, typeof firstCharacter);
+            } else if (i === input.length - 1) {
+                console.log('*****TRUE FALSE ELSE IF*****');
+                console.log('input.length ', input.length);
+                console.log('extractedCharacters ', extractedCharacters, typeof extractedCharacters);
                 console.log('i is currently ', i);
                 iterator = i;
-                firstNumber = parseInt(firstCharacter);
-                console.log('firstNumber after parseInt ', firstNumber, typeof firstCharacter);
+                extractedNumber = parseInt(extractedCharacters);
+                console.log('firstNumber after parseInt ', extractedNumber, typeof extractedCharacters);
                 break;
-
             } else {
-                console.log('firstCharacter ', firstCharacter, typeof firstCharacter);
+                console.log('extractedCharacters ', extractedCharacters, typeof extractedCharacters);
                 console.log('i is currently ', i);
                 iterator = i;
-                firstNumber = parseInt(firstCharacter);
-                console.log('firstNumber after parseInt ', firstNumber, typeof firstCharacter);
+                extractedNumber = parseInt(extractedCharacters);
+                console.log('firstNumber after parseInt ', extractedNumber, typeof extractedCharacters);
                 break;
             }
         } 
 
-        console.log(firstNumber, iterator);
-        returnArray[0] = firstNumber;
+        console.log(extractedNumber, iterator);
+        returnArray[0] = extractedNumber;
         returnArray[1] = iterator;
         return returnArray;
     }
 
-let oneNumber = findfirstCharacter();
+let oneNumber = findFirstNumber();
 console.log('oneNumber ', oneNumber, typeof oneNumber);
 
 simpleFunction();
