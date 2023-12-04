aa3ee895eed641eca828305de9d72e70e790538d
ttreit <14933277+ttreit@users.noreply.github.com>
2023-12-04 10:36:29 -0800

    Add return array

--- day1/code.js
+++ day1/code.js
@@ -2,15 +2,16 @@ function simpleFunction() {
     console.log('I am a simple function');
 }
 
-function findFirstNumber() {
+function findfirstCharacter() {
     //Variables
         let input;
-        let firstNumber = '0';
-        let text;
+        let firstCharacter = '0';
+        let firstNumber = 0;
+        let iterator = 0;
+        const returnArray = [firstNumber, iterator];
         const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-        let lastNumber;
-        let total;
-        input = 'a1562b0c2';
+
+        input = 'a12b0c2';
     
         for (let i = 0; i < input.length; i++) {
             console.log('input ', input);
@@ -18,21 +19,27 @@ function findFirstNumber() {
             console.log('iCharacter ', iCharacter);
             if (numbers.some(x => iCharacter.includes(x))) {
                 console.log('iCharacter contains a number');
-                firstNumber = firstNumber.concat(iCharacter);
-                console.log('firstNumber ', firstNumber, typeof firstNumber);
-            } else if (firstNumber === '0') {
+                firstCharacter = firstCharacter.concat(iCharacter);
+                console.log('firstCharacter ', firstCharacter, typeof firstCharacter);
+            } else if (firstCharacter === '0') {
                 continue;
             } else {
-                console.log('firstNumber ', firstNumber, typeof firstNumber);
+                console.log('firstCharacter ', firstCharacter, typeof firstCharacter);
                 console.log('i is currently ', i);
-                firstNumber = parseInt(firstNumber);
-                console.log('firstNumber after parseInt ', firstNumber, typeof firstNumber);
+                iterator = i;
+                firstNumber = parseInt(firstCharacter);
+                console.log('firstNumber after parseInt ', firstNumber, typeof firstCharacter);
                 break;
             }
         } 
-        return firstNumber;
+
+        console.log(firstNumber, iterator);
+        returnArray[0] = firstNumber;
+        returnArray[1] = iterator;
+        return returnArray;
     }
-    
-let oneNumber = findFirstNumber();
+
+let oneNumber = findfirstCharacter();
 console.log('oneNumber ', oneNumber, typeof oneNumber);
+
 simpleFunction();
