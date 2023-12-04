a515a5ae7cd943da37a9aef1d211a8ffea51fc5c
ttreit <14933277+ttreit@users.noreply.github.com>
2023-12-04 15:32:10 -0800

    Extract first and last digits from string

--- day1/code.js
+++ day1/code.js
@@ -6,34 +6,36 @@ function findFirstNumber() {
     //Variables
         let input;
         let extractedCharacters = '';
-        let extractedNumber = 0;
+        let extractedSecondCharacters = '';
+        let extractedNumber1;
+        let extractedNumber2;   
         let iterator = 0;
-        const returnArray = [extractedNumber, iterator];
+        const returnArray = [extractedNumber1, extractedNumber2];
         const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-        input = '12';
+        input = 'n1b265h75c7';
     
         for (let i = 0; i < input.length; i++) {
             console.log('input ', input);
             let iCharacter = input[i];
-            if (numbers.some(x => iCharacter.includes(x))) {
-                console.log('iCharacter contains the number ', iCharacter);
-                extractedCharacters = extractedCharacters.concat(iCharacter);
-                console.log('extractedCharacters ', extractedCharacters, typeof extractedCharacters);
-            } else if (extractedCharacters === '') {
-                continue;
-            } else {
-                console.log('**** Not a Number, Not Blank ****')
-                console.log('i =', i);
-                iterator = i;
-                break;
-            }
+            if (extractedCharacters === "" && numbers.some(x => iCharacter.includes(x))) {
+                console.log('IF #1 - iCharacter contains  ', iCharacter);
+                extractedCharacters = iCharacter;
+                console.log('IF #1 - extractedCharacters ', extractedCharacters, typeof extractedCharacters);
+            } else if (numbers.some(x => iCharacter.includes(x))) {
+                console.log('IF #2 - icharacter contains ', iCharacter);
+                extractedSecondCharacters = iCharacter;
+                console.log('IF #2 - extractedCharacters ', extractedCharacters, typeof extractedCharacters);
+                console.log('IF #2 - extractedSecondCharacters ', extractedSecondCharacters, typeof extractedSecondCharacters);          
+            } 
         } 
         //parseInt 
-        extractedNumber = parseInt(extractedCharacters);
-        console.log(extractedNumber, iterator);
-        returnArray[0] = extractedNumber;
-        returnArray[1] = iterator;
+        extractedNumber1 = parseInt(extractedCharacters);
+        console.log('extractedNumber1 ', extractedNumber1);
+        extractedNumber2 = parseInt(extractedSecondCharacters);
+        console.log('extractedNumber2 ', extractedNumber2);
+        returnArray[0] = extractedNumber1;
+        returnArray[1] = extractedNumber2;
         return returnArray;
     }
 
